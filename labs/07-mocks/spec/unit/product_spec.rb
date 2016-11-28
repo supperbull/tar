0edb0d76fce677cc6c8937a10396e32846b09d8a
Wlodek Bzyl <matwb@univ.gda.pl>
2016-11-28 13:11:29 +0100

    update

---
+++ labs/07-mocks/spec/unit/product_spec.rb
@@ -0,0 +1,13 @@
+require 'product'
+
+RSpec.describe Product do
+  it '#name' do
+    product = instance_double('product', name: 'beer')
+    expect(product.name).to eq 'beer'
+  end
+  it '#price' do
+    money = instance_double('money', currency: 'USD', value: 10)
+    product = instance_double('product', price: money)
+    expect(product.price.value).to eq 10
+  end
+end
